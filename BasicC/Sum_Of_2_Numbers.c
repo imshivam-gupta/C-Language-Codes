4261102a21eb97bfd9f4a9287a7a694324857868
Shivam Gupta <93479496+Shivamgupta-2003@users.noreply.github.com>
2022-01-07 15:33:05 +0530

    Create Sum_Of_2_Numbers.c

---
+++ BasicC/Sum_Of_2_Numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main()
+{
+   int integer1;
+   int integer2;
+   
+   printf("Enter 1st Number\a\n");
+   scanf("%d",&integer1);
+   
+   printf("Enter 2nd Number\a\n");
+   scanf("%d",&integer2);
+   
+   int sum=integer1+integer1;
+   
+   printf("Required Sum is %d\n",sum);
+   
+   
+}
