51e880c3667e8bbd4d0474f5dec97bf5b093c40c
Shivam Gupta <93479496+ShivamGupta2003@users.noreply.github.com>
2022-03-04 15:29:19 +0530

    Create 01. Fibonacci with and without Recursion.c

---
+++ Mix Problem Practice/01. Fibonacci with and without Recursion.c
@@ -0,0 +1,41 @@
+// Without Recursion Printing
+
+#include<stdio.h>    
+int main()    
+{    
+ int n1=0,n2=1,n3,i,number;    
+ printf("Enter the number of elements:");    
+ scanf("%d",&number);    
+ printf("\n%d %d",n1,n2);//printing 0 and 1    
+ for(i=2;i<number;++i)//loop starts from 2 because 0 and 1 are already printed    
+ {    
+  n3=n1+n2;    
+  printf(" %d",n3);    
+  n1=n2;    
+  n2=n3;    
+ }  
+  return 0;  
+}
+  
+  // With Recursion printing
+
+#include<stdio.h>    
+void printFibonacci(int n){    
+    static int n1=0,n2=1,n3;    
+    if(n>0){    
+         n3 = n1 + n2;    
+         n1 = n2;    
+         n2 = n3;    
+         printf("%d ",n3);    
+         printFibonacci(n-1);    
+    }    
+}    
+int main(){    
+    int n;    
+    printf("Enter the number of elements: ");    
+    scanf("%d",&n);    
+    printf("Fibonacci Series: ");    
+    printf("%d %d ",0,1);    
+    printFibonacci(n-2);//n-2 because 2 numbers are already printed    
+  return 0;  
+ }    
